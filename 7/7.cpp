4838d7d8cf5b9f5526daccd01412c26f4b4e901f
Calin Cruceru <calin@rosedu.org>
2014-11-07 14:35:12 +0200

    Add 7

--- 7/7.cpp
+++ 7/7.cpp
@@ -1,10 +1,15 @@
 #include <fstream>
+#include <string>
 
 
 int main()
 {
     std::ifstream fin("Input7.txt");
     std::ofstream fout("Output7.txt");
+
+    std::string str;
+    std::getline(fin, str);
+
 
 
     return 0;
